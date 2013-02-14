0a08e0c0ed26437e4f529692b0f540ff64bb8967
mayram <mayram@4a0602ed-0ddf-4d1e-8465-5e6d797b7344>
2013-02-14 15:32:45 +0000

    git-svn-id: https://scm.gforge.inria.fr/svn/easycrypt/branches/modules@2577 4a0602ed-0ddf-4d1e-8465-5e6d797b7344

--- webui/frontend/easycrypt/easycrypt.js
+++ webui/frontend/easycrypt/easycrypt.js
@@ -1,3 +1,11 @@
+// --------------------------------------------------------------------
+Array.prototype.peek = function() {
+	if (this.length > 0)
+		return this[this.length-1];
+	return undefined;
+}
+
+// --------------------------------------------------------------------
 function EasyCryptEditor(name) {
     this.widgets = {
         prev    : $('#' + name + "-prev"),
@@ -7,10 +15,11 @@ function EasyCryptEditor(name) {
         feedback: $('#' + name + "-feedback"),        
     };
 
-    this.name     = name;
-    this.position = -1;
-    this.romark   = null;
-    this.editor   = null;
+    this.name      = name;
+    this.endofsent = [];
+    this.init      = 0;
+    this.romark    = null;
+    this.editor    = null;
 
     this.createWidget();
 }
@@ -52,57 +61,84 @@ EasyCryptEditor.prototype.clearROMark = function() {
 
 EasyCryptEditor.prototype.setROMark = function(end) {
     var opts = {
-        className: this.name + '-read-only',
-         readOnly: true,
+           className: this.name + '-read-only',
+            readOnly: true,
+      inclusiveRight: false,
     };
 
     this.clearROMark();
-    this.romark = this.editor.markText({ line: 0, ch: 0 }, end, opts);
+    if (end)
+    	this.romark = this.editor.markText({ line: 0, ch: 0 }, end, opts);
 }
 
 // --------------------------------------------------------------------
 // Buttons callbacks
 EasyCryptEditor.prototype._on_test = function() {
-    var line = this.editor.getLine(0);
-    var l = 0;
-    var statement;
-    for (var i = 1; i < line.length; i++) {
-    	var o = this.editor.getTokenAt({ line: l, ch: i});
-    	if (o.type == "punctuation" && o.string == '.') {
-    		statement = line.substr(0,i);
-    	}
-    }
-    alert(statement);
-    //alert(o.start + " " + o.end + " " + o.string + " " + o.type);
+	var myWebSocket = new WebSocket("ws://echo.websocket.org");
+	var message = "what do you want to send";
+	myWebSocket.onopen = function(evt) { 
+		alert("Connection is opening...");
+		alert("SENT : " + message);
+		myWebSocket.send(message);
+	};
+	myWebSocket.onclose = function(evt) { alert("Connection is closed"); };
+	myWebSocket.onmessage = function(evt) { 
+		alert("SERVER: " + evt.data);
+		myWebSocket.close();
+		};
 }
 
 EasyCryptEditor.prototype._on_next = function() {
-    if (this.position < this.editor.lineCount()-1) {
-        this.position++;
-        var line = this.editor.getLine(this.position);
-        this.widgets.feedback
-            .append("STATEMENT SENT: " + (this.position+1) +
-                    " " + line + "\n");
-    }
-    this.setROMark({ line: this.position+1, ch: 0 });
+	prev = this.endofsent.peek() || {line: 0, ch: 1};
+	end  = this.findStatement(prev);
+
+	if (end) {
+		this.widgets.feedback
+			.append($.format("STATEMENT SENT: {0} {1}\n",
+					         end.line+1, end.contents));
+		this.endofsent.push(end);
+		this.setROMark(end);
+	}
 }
 
 EasyCryptEditor.prototype._on_prev = function() {
-    if (this.position >= 0) {
-        this.position--;
-        var line = this.editor.getLine(this.position);
-        this.widgets.feedback
-            .append("LINE SENT: " + (this.position+1) +
-                    " " + line + "\n");
-        this.setROMark({ line: this.position+1, ch: 0 });
-    }
+	if (this.endofsent.length == 0)
+		return ;
+
+	var posend   = this.endofsent.pop();
+	var posstart = this.endofsent.peek();
+
+	this.setROMark(posstart);
+
+	this.widgets.feedback
+    	.append($.format("LINE SENT: {0} {1}\n",
+    					 posend.line+1, posend.contents));
 }
 
 // --------------------------------------------------------------------
-EasyCryptEditor.prototype.findStatement = function(line) {
-    var counter = line.length-1;
-    for (var c = 0; c < counter; c++ ) {
-        if (line[c] == '.' && line[c+1] == ' ')
-        	return line.substr(0,c);
-    }
+EasyCryptEditor.prototype.findStatement = function(position) {
+	var contents = "";
+	var current  = {     line: position.line ,
+			               ch: position.ch   ,
+			         contents: ""            };
+	
+	while (current.line < this.editor.lineCount()) {
+		if (current.ch-1 == this.editor.getLine(current.line).length) {
+			contents += " ";
+			++current.line;
+			current.ch = 1;
+			continue ;
+		}
+
+		var token = this.editor.getTokenAt(current);
+
+		contents += token.string;
+		current.ch = token.end+1;
+		if (token.string == "." && token.type == "punctuation") {
+			current.contents = contents;			
+			break ;
+		}
+	}
+
+	return current.contents ? current : undefined;
 }
