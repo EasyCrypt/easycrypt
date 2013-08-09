6775c03c81a0f93346ab2dcdd36b38a73d6c819f
Benedikt Schmidt <beschmi@gmail.com>
2013-08-09 16:59:39 +0200

    fix extract_modules script

--- examples/AKE/extract_modules.py
+++ examples/AKE/extract_modules.py
@@ -6,27 +6,28 @@ import os
 def get_modules(filename):
   start_re = re.compile('^module[\s]+([\w]+)')
   end_re = re.compile('^\}.')
-  module = {}
   in_module = False
   with open(filename) as file_contents:
     for line in file_contents:
       res = start_re.findall(line)
-      if len(res) == 1:
-        #print "START"
-        #print res[0]
+      if len(res) == 1 and res[0] != "type" and (not in_module):
+        # print "START: " + res[0]
+        module = {}
         module['name'] = res[0]
         module['content'] = [line]
         in_module = True
       elif in_module:
         module['content'].append(line)
         if end_re.match(line):
-          #print "END"
+          # print "END"
           in_module = False
           yield module
 
 modules = list(get_modules('AKE_proof.ec'))
+
 if not (os.path.isdir('modules')):
   os.mkdir('modules')
+
 for m in modules:
   filename = "modules/%s.ec" % m['name']
   print "Writing file `%s'" % filename
