d499ae6bb781b34850cb517d981f62f852c69526
Benjamin Gregoire <Benjamin.Gregoire@inria.fr>
2013-10-23 17:16:44 +0200

    Fixing bug in interface tarray -> array

--- extraction/ecFArray.mli
+++ extraction/ecFArray.mli
@@ -1,77 +1,77 @@
 (* Array.array *)
-type 'x tarray
+type 'x array
 
-val array: 'x array -> 'x tarray
+val array: 'x array -> 'x array
   
 (* Array.length *)
-val length : 'x tarray -> EcPervasive.int0 
+val length : 'x array -> EcPervasive.int0 
   
 (* Array.`|_| *)
-val bqbr_br : 'x tarray -> EcPervasive.int0 
+val bqbr_br : 'x array -> EcPervasive.int0 
   
 (* Array._.[_] *)
-val _dtlb_rb : 'x tarray -> EcPervasive.int0 -> 'x 
+val _dtlb_rb : 'x array -> EcPervasive.int0 -> 'x 
   
 (* Array.empty *)
-val empty : 'x tarray 
+val empty : 'x array 
   
 (* Array._::_ *)
-val _clcl_ : 'x -> 'x tarray -> 'x tarray
+val _clcl_ : 'x -> 'x array -> 'x array
   
 (* Array.::: *)
-val clclcl : 'x tarray -> 'x -> 'x tarray 
+val clclcl : 'x array -> 'x -> 'x array 
   
 (* Array._.[_<-_] *)
-val _dtlb_lsmn_rb : 'x tarray -> EcPervasive.int0 -> 'x -> 'x tarray 
+val _dtlb_lsmn_rb : 'x array -> EcPervasive.int0 -> 'x -> 'x array 
   
 (* Array.make *)
-val make : EcPervasive.int0 -> 'x -> 'x tarray 
+val make : EcPervasive.int0 -> 'x -> 'x array 
   
 (* Array.init *)
-val init : EcPervasive.int0 -> (EcPervasive.int0 -> 'x) -> 'x tarray 
+val init : EcPervasive.int0 -> (EcPervasive.int0 -> 'x) -> 'x array 
   
 (* Array.|| *)
-val brbr : 'x tarray -> 'x tarray -> 'x tarray 
+val brbr : 'x array -> 'x array -> 'x array 
   
 (* Array.sub *)
-val sub : 'x tarray -> EcPervasive.int0 -> EcPervasive.int0 -> 'x tarray 
+val sub : 'x array -> EcPervasive.int0 -> EcPervasive.int0 -> 'x array 
   
 (* Array.fill *)
-val fill : 'x tarray ->
-               EcPervasive.int0 -> EcPervasive.int0 -> 'x -> 'x tarray 
+val fill : 'x array ->
+               EcPervasive.int0 -> EcPervasive.int0 -> 'x -> 'x array 
   
 (* Array.blit *)
-val blit : 'x tarray ->
+val blit : 'x array ->
                EcPervasive.int0 ->
-                 'x tarray -> EcPervasive.int0 -> EcPervasive.int0 -> 'x tarray 
+                 'x array -> EcPervasive.int0 -> EcPervasive.int0 -> 'x array 
   
 (* Array.map *)
-val map : ('x -> 'y) -> 'x tarray -> 'y tarray 
+val map : ('x -> 'y) -> 'x array -> 'y array 
   
 (* Array.map2 *)
-val map2 : ('x -> 'y -> 'z) -> 'x tarray -> 'y tarray -> 'z tarray 
+val map2 : ('x -> 'y -> 'z) -> 'x array -> 'y array -> 'z array 
   
 (* Array.mapi *)
-val mapi : (EcPervasive.int0 -> 'x -> 'y) -> 'x tarray -> 'y tarray 
+val mapi : (EcPervasive.int0 -> 'x -> 'y) -> 'x array -> 'y array 
   
 (* Array.fold_left *)
-val fold_left : ('state -> 'x -> 'state) -> 'state -> 'x tarray -> 'state 
+val fold_left : ('state -> 'x -> 'state) -> 'state -> 'x array -> 'state 
   
 (* Array.fold_right *)
-val fold_right : ('state -> 'x -> 'state) -> 'state -> 'x tarray -> 'state 
+val fold_right : ('state -> 'x -> 'state) -> 'state -> 'x array -> 'state 
   
 (* Array.all *)
-val all : ('x -> EcPervasive.bool0) -> 'x tarray -> EcPervasive.bool0 
+val all : ('x -> EcPervasive.bool0) -> 'x array -> EcPervasive.bool0 
   
 (* Array.alli *)
 val alli : (EcPervasive.int0 -> 'x -> EcPervasive.bool0) ->
-               'x tarray -> EcPervasive.bool0 
+               'x array -> EcPervasive.bool0 
   
 (* Array.Darray *)
 module Darray : sig
   
   (* Array.Darray.darray *)
   val darray : EcPervasive.int0 ->
-                   'a EcPervasive.distr -> 'a tarray EcPervasive.distr 
+                   'a EcPervasive.distr -> 'a array EcPervasive.distr 
 
 end
