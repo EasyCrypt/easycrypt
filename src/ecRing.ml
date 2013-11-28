a191a249fe0355753ba2ce7bcb0ae016c9cbdff2
Benjamin Gregoire <Benjamin.Gregoire@inria.fr>
2013-11-28 11:29:09 +0100

    add printer for debugging and translation from pol to pexpr

--- src/ecRing.ml
+++ src/ecRing.ml
@@ -23,7 +23,8 @@ let copp a =  minus_big_int a
 let ceq a b = eq_big_int a b
 let cdiv a b = quomod_big_int a b
 
-type pexpr = PEc of c 
+type pexpr = 
+  | PEc of c 
   | PEX of int
   | PEadd of pexpr * pexpr 
   | PEsub of pexpr * pexpr
@@ -31,12 +32,72 @@ type pexpr = PEc of c
   | PEopp of pexpr 
   | PEpow of pexpr * int
 
-type pol = Pc of c 
+let rec pp_pe fmt = function
+  | PEc c -> Format.fprintf fmt "%s" (string_of_big_int c)
+  | PEX i -> Format.fprintf fmt "x_%i" i
+  | PEadd(p1,p2) -> Format.fprintf fmt "%a + %a" pp_pe p1 pp_pe p2
+  | PEsub(p1,p2) -> Format.fprintf fmt "%a - %a" pp_pe p1 pp_pe p2
+  | PEmul(p1,p2) -> Format.fprintf fmt "%a * %a" pp_pe p1 pp_pe p2
+  | PEopp p -> Format.fprintf fmt "-%a" pp_pe p
+  | PEpow(p,i) -> Format.fprintf fmt "%a^%i" pp_pe p i
+  
+type pol =
+  | Pc of c 
   | Pinj of int * pol
   | PX of pol * int * pol
 
 type mon = Mon0 | Zmon of (int * mon) | Vmon of (int * mon)
 
+(*let rec pp_pol fmt = function
+  | Pc c -> Format.fprintf fmt "%s" (string_of_big_int c)
+  | Pinj(i,p) -> Format.fprintf fmt "%i(%a)" i pp_pol p
+  | PX(q,i,p) -> Format.fprintf fmt "(%a^%i + %a)" pp_pol q i pp_pol p
+
+let rec pp_mon c fmt = function
+  | Mon0 -> (*Format.fprintf fmt "%s" (string_of_big_int c)*) ()
+  | Zmon(i,p) -> Format.fprintf fmt "%i(%a)" i (pp_mon c) p
+  | Vmon(i,p) -> Format.fprintf fmt "(^%i)%a" i  (pp_mon c) p *)
+
+let ofpol (p:pol) = 
+  let mkxp i p mon = 
+    if p = 0 then mon 
+    else if p = 1 then (PEX i) :: mon
+    else PEpow(PEX i, p) :: mon in
+  let iszero pe = match pe with PEc c -> ceq c c0 | _ -> false in
+  let mkmul c mon = 
+    if ceq c c0 || mon = [] then PEc c 
+    else 
+      let hd,tl = 
+        if ceq c c1 then List.hd mon, List.tl mon
+        else PEc c, mon in
+      List.fold_left (fun p x -> PEmul(p, x)) hd tl in
+  let mkadd r neg p = 
+    if iszero p then r
+    else
+      if iszero r then if neg then PEopp p else p
+      else if neg then PEsub(r, p) else PEadd(r,p) in
+  let rec aux q i p mon r = (* q * x_i^p * mon + r *)
+    match q with
+    | Pc c ->
+      let mon = mkxp i p mon in (* x_i * mon *)
+      let neg, q = 
+        if sign_big_int c = -1 then true, mkmul (copp c) mon
+        else false, mkmul c mon in
+      mkadd r neg q 
+    | Pinj(j,q) -> aux q (i+j) 0 (mkxp i p mon) r
+    | PX(q1,p',q2) -> 
+      let r = aux q2 (i+1) 0 (mkxp i p mon) r in
+      aux q1 i (p+p') mon r in
+  aux p 0 0 [] (PEc c0)
+
+
+let ofmon c mon = 
+  let rec aux q i mon =
+    match mon with
+    | Mon0 -> q
+    | Zmon(j,mon) -> aux q (i+j) mon
+    | Vmon(p,mon) -> aux (PEmul(q,PEpow(PEX i,p))) (i+1) mon in
+  aux (PEc c) 0 mon
 
 let rec pexpr_eq (e1 : pexpr) (e2 : pexpr) : bool =
   match (e1,e2) with
