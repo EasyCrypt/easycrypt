97701eb8aea7872c5b42646238eefbd07fc79694
Benjamin Gregoire <Benjamin.Gregoire@inria.fr>
2013-12-11 12:50:52 +0100

    add comments

--- src/phl/ecPhlTrans.mli
+++ src/phl/ecPhlTrans.mli
@@ -7,6 +7,21 @@ open EcBaseLogic
 open EcLogic
 
 (* -------------------------------------------------------------------- *)
+(* -------------------------------------------------------------------- *)
+(* Transitivity rule for equiv                                          *)
+
+(*  forall m1 m3, P m1 m3 => exists m2, P1 m1 m2 /\ P2 m2 m3
+ *  forall m1 m2 m3, Q1 m1 m2 => Q2 m2 m3 => Q m1 m3
+ *  c1 ~ c2 : P1 ==> Q1
+ *  c2 ~ c3 : P2 ==> Q2
+ *  --------------------------------------------------------
+ *      c1 ~ c3 : P ==> Q
+ * Remark: the most basic rule is normally 
+ *   Q = exists m2, Q1 m1 m2 /\ Q2 m2 m3
+ * => the actual rule is in fact this basic rule + conseq
+ * I prefer this more convenient one
+ *)
+
 class rn_equiv_trans : object
   inherit xrule
 end
