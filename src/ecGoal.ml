4dee4a422749df6ed43e1011e4812bbef3a17544
Pierre-Yves Strub <pierre-yves@strub.nu>
2014-02-24 16:07:08 +0100

    More on the new proof-engine.
    Basic goals manipulation, including goal validation.

--- src/ecGoal.ml
+++ src/ecGoal.ml
@@ -1,4 +1,5 @@
 (* -------------------------------------------------------------------- *)
+open EcUtils
 open EcIdent
 open EcTypes
 open EcFol
@@ -10,36 +11,21 @@ module ID : sig
 
   val gen : unit -> int
 
-  module Map : EcMaps.Map.S with type key = id
+  module Map : EcMaps.Map.S with type key   = id
+  module Set : EcMaps.Set.S with type M.key = id
 end = struct
   type id = int
 
   let gen () = EcUid.unique ()
 
   module Map = EcMaps.Mint
+  module Set = EcMaps.Sint
 end
 
 (* -------------------------------------------------------------------- *)
 type handle = ID.id
 
 (* -------------------------------------------------------------------- *)
-(* EasyCrypt proof-term:                                                *)
-(*                                                                      *)
-(*  pt ::= pt-head pt-arg*                                              *)
-(*                                                                      *)
-(*  pt-head ::=                                                         *)
-(*   |  form<ft>                   (cut <ft> - generate subgoal         *)
-(*   |  handle                     (formula associated to <handle>)     *)
-(*   |  local<id>                  (local hypothesis <id>)              *)
-(*   |  global<p,tyargs>           (global lemma <p<:tyargs>>)          *)
-(*                                                                      *)
-(* pt-arg ::=                                                           *)
-(*   | formula                     (∀-elimination)                      *)
-(*   | memory                      (∀[mem]-elimination)                 *)
-(*   | module-path                 (∀[mod]-elimination)                 *)
-(*   | pt                          (⇒-elimination)                      *)
-(* -------------------------------------------------------------------- *)
-
 type proofterm = { pt_head : pt_head; pt_args : pt_arg list; }
 
 and pt_head =
@@ -55,11 +41,13 @@ and pt_arg =
 | PASub     of proofterm
 
 (* -------------------------------------------------------------------- *)
-(* EasyCrypt rewrite proof-term:                                        *)
-(*  rwpt := pt * position list                                          *)
-(*    <pt>: equality-proof term                                         *)
-(*                                                                      *)
-(*  position := term position (to be defined)                           *)
+module PT : sig
+  val check : LDecl.hyps -> proofterm -> form
+end = struct
+  let check (_hyps : LDecl.hyps) (_pt : proofterm) =
+    assert false
+end
+
 (* -------------------------------------------------------------------- *)
 type position
 
@@ -70,8 +58,8 @@ type rwproofterm = {
 
 (* -------------------------------------------------------------------- *)
 type proof = {
-  pr_env   : proofenv;
-  pr_goals : handle list;
+  pr_env    : proofenv;
+  pr_opened : handle list;
 }
 
 and proofenv = {
@@ -88,22 +76,32 @@ and pregoal = {
 
 and goal = {
   g_goal       : pregoal;
-  g_validation : validation;
+  g_validation : validation option;
 }
 
 and validation =
 | VSmt     : validation                 (* SMT call *)
 | VAdmit   : validation                 (* admit *)
-| VConv    : Sid.t                      (* weakening + conversion *)
+| VConv    : Sid.t -> validation        (* weakening + conversion *)
 | VApply   : proofterm -> validation    (* modus ponens *)
 | VRewrite : rwproofterm -> validation  (* rewrite *)
 | VExtern  : 'a -> validation           (* external (hl/phl/prhl/...) proof-node *)
 
 and tcenv = {
   tce_proofenv   : proofenv;           (* top-level proof environment *)
-  tce_goal       : pregoal;            (* local goal *)
-  tce_subgoals   : pregoal list;       (* generated subgoals *)
-  tce_validation : validation option;  (* pending validation *)
+  tce_goal       : pregoal option;     (* current goal *)
+  tce_subgoals   : tcenv_ctxt;         (* subgoals (zipper) *)
+}
+
+and tcenv_ctxt = {
+  tcec_closed : ID.Set.t;
+  tcec_gzip   : tcenv_gzip;
+}
+
+and tcenv_gzip = {
+  tcez_left  : handle list;
+  tcez_right : handle list;
+  tcez_ctxt  : [`Top | `Intern of tcenv_gzip];
 }
 
 and location = {
@@ -113,45 +111,139 @@ and location = {
 }
 
 (* -------------------------------------------------------------------- *)
-(* Functional API                                                       *)
-(* -------------------------------------------------------------------- *)
-module type Api = sig
-  (* - [forward tactic]: take a proofenv, i.e. a set of goals (proven or
-   *   not) and generate a new (1-level proven) goal [handle]. Examples
-   *   of such tactics are forward congruence or closed rewriting.
-   *
-   * - [backward tactic]: take a tcenv, i.e. a opened goal, and make
-   *   progress over it, potentially creating new subgoals. Examples of
-   *   such tactics are backward apply, rewriting, backward congruence.
-   *
-   * - [mixward tactic]: take a tcenv a apply a forward tactic to its
-   *   associated proof-environment, potentially creating subgoals.
-   *   Examples of such tactics are forward apply, conditional closed
-   *   rewriting.
-   *)
-
+module Api = struct
   type 'a forward  = 'a -> proofenv -> proofenv * handle
   type 'a backward = 'a -> tcenv -> tcenv
   type 'a mixward  = 'a -> tcenv -> tcenv * handle
 
-  val create : LDecl.hyps -> form -> proof
+  exception InvalidStateException of string
 
-  val newgoal : tcenv -> ?hyps:LDecl.hyps -> form -> tcenv * handle
-  val close   : tcenv -> validation -> tcenv
+  let newgoal (tc : tcenv) ?(hyps:LDecl.hyps option) (goal : form) =
+    assert false
+
+  (* ------------------------------------------------------------------ *)
+  let get_goal_by_id (hd : handle) (pe : proofenv) =
+    match ID.Map.find_opt hd pe.pr_goals with
+    | None    -> raise (InvalidStateException "goal-map-inconsistent")
+    | Some pr -> pr
+
+  (* ------------------------------------------------------------------ *)
+  let get_pregoal_by_id (hd : handle) (pe : proofenv) =
+    (get_goal_by_id hd pe).g_goal
+
+
+  (* ------------------------------------------------------------------ *)
+  let update_goal_map (tx : goal -> goal) (hd : handle) (pe : proofenv) =
+    let change g =
+      match g with
+      | None   -> raise (InvalidStateException "goal-map-inconsistent")
+      | Some g -> Some (tx g)
+    in
+      { pe with pr_goals = ID.Map.change change hd pe.pr_goals }
+
+  (* ------------------------------------------------------------------ *)
+  let update_goal_map_on_tcenv (tx : goal -> goal) (hd : handle) (tc : tcenv) =
+    { tc with tce_proofenv = update_goal_map tx hd tc.tce_proofenv }
+
+  (* ------------------------------------------------------------------ *)
+  let check_tcenv_opened (tc : tcenv) =
+    if tc.tce_goal = None then
+      raise (InvalidStateException "all-goals-closed")
+
+  (* ------------------------------------------------------------------ *)
+  let reduce_tcenvx (tcx : tcenv_ctxt) =
+    if ID.Set.is_empty tcx.tcec_closed then
+      tcx
+    else
+      let filter =
+        List.fold_right
+          (fun id (ids, gs) ->
+            if   ID.Set.mem id ids
+            then (ID.Set.remove id ids, gs)
+            else (ids, id :: gs))
+      in
+
+      let ids = tcx.tcec_closed in
+      let ids, tcez_left  = filter tcx.tcec_gzip.tcez_left  (ids, []) in
+      let ids, tcez_right = filter tcx.tcec_gzip.tcez_right (ids, []) in
+        { tcec_closed = ids;
+          tcec_gzip   = { tcx.tcec_gzip with tcez_left; tcez_right; } }
+
+  (* ------------------------------------------------------------------ *)
+  let pop_from_tcenvx (tcx : tcenv_ctxt) =
+    let tcx = reduce_tcenvx tcx in
+    let tcz = tcx.tcec_gzip in
+
+    let (sg, tcz) =
+      match tcz.tcez_right with
+      | sg :: rgs -> (Some sg, { tcz with tcez_right = rgs })
+      | [] ->
+          match tcz.tcez_left with
+          | sg :: lgs -> (Some sg, { tcz with tcez_left = lgs })
+          | [] -> (None, tcz)
+    in
+      (sg, { tcx with tcec_gzip = tcz })
+
+  (* ------------------------------------------------------------------ *)
+  let push_closed_to_tcenvx (hd : handle) (tcx : tcenv_ctxt) =
+    { tcx with tcec_closed = ID.Set.add hd tcx.tcec_closed }
+
+  (* ------------------------------------------------------------------ *)
+  let close (tc : tcenv) (vx : validation) =
+     check_tcenv_opened tc;
+
+    let current = oget tc.tce_goal in
+
+    let change g =
+      if g.g_validation <> None || g.g_goal != current then
+        raise (InvalidStateException "goal-map-inconsistent");
+      { g with g_validation = Some vx }
+    in
+
+    (* Close current goal, set focused goal to None *)
+    let tc = update_goal_map_on_tcenv change current.g_uid tc in
+    let tc = { tc with tce_goal = None } in
+
+    (* Maybe pop one opened goal from proof context *)
+    let tc =
+      let (sg, tcx) = pop_from_tcenvx tc.tce_subgoals in
+        { tc with
+            tce_goal     = sg |> omap (get_pregoal_by_id^~ tc.tce_proofenv);
+            tce_subgoals = tcx; }
+    in
+
+    (* Register current goal has being closed *)
+    { tc with tce_subgoals = push_closed_to_tcenvx current.g_uid tc.tce_subgoals }
+
+  (* Tacticals *)
+  type ontest    = int -> proofenv -> handle -> bool
+  type direction = [ `Left | `Right ]
+
+  let on (tt : 'a backward) (f : ontest) (x : 'a) (tc : tcenv) =
+    assert false
+
+  let first (tt : 'a backward) (i : int) (x : 'a) (tc : tcenv) =
+    assert false
+
+  let last (tt : 'a backward) (i : int) (x : 'a) (tc : tcenv) =
+    assert false
+
+  let rotate (d : direction) (i : int) (tc : tcenv) =
+    assert false
+
+  let seq (tt1 : 'a backward) (tt2 : 'a backward) (tc : tcenv) =
+    assert false
+
+  let lseq (tts : 'a backward list) (tc : tcenv) =
+    assert false
 end
 
-(* -------------------------------------------------------------------- *)
-(* Imperative API                                                       *)
 (* -------------------------------------------------------------------- *)
 module type IApi = sig
   type rprooftree
   type rtcenv
   type rtcenvs
 
-  (* Same API as [Api], but mutating the state. We also provide
-   * a freeze/thaw couple of operations. *)
   val freeze : rtcenv -> rtcenv
   val thaw   : rtcenv -> rtcenv
-
-  (* + missing syntax for easing goals manipulations (using ocaml ppx) *)
 end
---
+++ src/ecGoal.mli
@@ -0,0 +1,127 @@
+(* -------------------------------------------------------------------- *)
+open EcIdent
+open EcTypes
+open EcEnv
+open EcFol
+
+(* -------------------------------------------------------------------- *)
+(* Proof-node ID                                                        *)
+(* -------------------------------------------------------------------- *)
+type handle
+
+(* -------------------------------------------------------------------- *)
+(* EasyCrypt proof-term:                                                *)
+(*                                                                      *)
+(*  pt ::= pt-head pt-arg*                                              *)
+(*                                                                      *)
+(*  pt-head ::=                                                         *)
+(*   |  form<ft>                   (cut <ft> - generate subgoal)        *)
+(*   |  handle                     (formula associated to <handle>)     *)
+(*   |  local<id>                  (local hypothesis <id>)              *)
+(*   |  global<p,tyargs>           (global lemma <p<:tyargs>>)          *)
+(*                                                                      *)
+(* pt-arg ::=                                                           *)
+(*   | formula                     (∀-elimination)                      *)
+(*   | memory                      (∀[mem]-elimination)                 *)
+(*   | module-path                 (∀[mod]-elimination)                 *)
+(*   | pt                          (⇒-elimination)                      *)
+(* -------------------------------------------------------------------- *)
+
+type proofterm = { pt_head : pt_head; pt_args : pt_arg list; }
+
+and pt_head =
+| PTCut    of EcFol.form
+| PTHandle of handle
+| PTLocal  of EcIdent.t
+| PTGlobal of EcPath.path * (ty list)
+
+and pt_arg =
+| PAFormula of EcFol.form
+| PAMemory  of EcMemory.memory
+| PAModule  of (EcPath.mpath * EcModules.module_sig)
+| PASub     of proofterm
+
+(* -------------------------------------------------------------------- *)
+(* EasyCrypt rewrite proof-term:                                        *)
+(*  rwpt := pt * position list                                          *)
+(*    <pt>: equality-proof term                                         *)
+(*                                                                      *)
+(*  position := term position (to be defined)                           *)
+(* -------------------------------------------------------------------- *)
+type position
+
+type rwproofterm = {
+  rpt_proof : proofterm;
+  rtp_occrs : position list;
+}
+
+(* -------------------------------------------------------------------- *)
+type proof
+type proofenv
+type tcenv
+
+type validation =
+| VSmt     : validation                 (* SMT call *)
+| VAdmit   : validation                 (* admit *)
+| VConv    : Sid.t -> validation        (* weakening + conversion *)
+| VApply   : proofterm -> validation    (* modus ponens *)
+| VRewrite : rwproofterm -> validation  (* rewrite *)
+| VExtern  : 'a -> validation           (* external (hl/phl/prhl/...) proof-node *)
+
+
+(* -------------------------------------------------------------------- *)
+(* Functional API                                                       *)
+(* -------------------------------------------------------------------- *)
+module Api : sig
+  (* - [forward tactic]: take a proofenv, i.e. a set of goals (proven or
+   *   not) and generate a new (1-level proven) goal [handle]. Examples
+   *   of such tactics are forward congruence or closed rewriting.
+   *
+   * - [backward tactic]: take a tcenv, i.e. an opened goal, and make
+   *   progress over it, potentially creating new subgoals. Examples of
+   *   such tactics are backward apply, rewriting, backward congruence.
+   *
+   * - [mixward tactic]: take a tcenv a apply a forward tactic to its
+   *   associated proof-environment, potentially creating subgoals.
+   *   Examples of such tactics are forward apply, conditional closed
+   *   rewriting.
+   *)
+
+  exception InvalidStateException of string
+
+  type 'a forward  = 'a -> proofenv -> proofenv * handle
+  type 'a backward = 'a -> tcenv -> tcenv
+  type 'a mixward  = 'a -> tcenv -> tcenv * handle
+
+  val newgoal : tcenv -> ?hyps:LDecl.hyps -> form -> tcenv * handle
+  val close   : tcenv -> validation -> tcenv
+
+  (* Tacticals *)
+  type ontest    = int -> proofenv -> handle -> bool
+  type direction = [ `Left | `Right ]
+
+  val on     : 'a backward -> ontest -> 'a -> tcenv -> tcenv
+  val first  : 'a backward -> int -> 'a -> tcenv -> tcenv
+  val last   : 'a backward -> int -> 'a -> tcenv -> tcenv
+  val rotate : direction -> int -> tcenv -> tcenv
+
+  val seq  : 'a backward -> 'a backward -> tcenv -> tcenv
+  val lseq : 'a backward list -> tcenv -> tcenv
+end
+
+
+(* -------------------------------------------------------------------- *)
+(* Imperative API                                                       *)
+(* -------------------------------------------------------------------- *)
+module type IApi = sig
+  type rprooftree
+  type rtcenv
+  type rtcenvs
+
+  (* Same API as [Api], but mutating the state. We also provide
+   * a freeze/thaw couple of operations. *)
+  val freeze : rtcenv -> rtcenv
+  val thaw   : rtcenv -> rtcenv
+
+  (* + missing syntax for easing goals manipulations (using ocaml ppx) *)
+end
