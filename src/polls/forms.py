1e0325c936f7a8c75e72453a35278bc6b80c86f9
Guillermo Ramos <guillermo.ramos@imdea.org>
2014-03-26 10:49:38 +0100

    Integrated crispy_forms with bootstrap3

---
+++ src/polls/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from django.core.urlresolvers import reverse
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+
+class ContactForm(forms.Form):
+    like_website = forms.TypedChoiceField(
+        label="Do you like this website?",
+        choices=((1, "Yes"), (0, "No")),
+        coerce=lambda x: bool(int(x)),
+        widget=forms.RadioSelect,
+        initial='1',
+        required=True,
+    )
+
+    favorite_food = forms.CharField(
+        label="What is your favorite food?",
+        max_length=80,
+        required=True,
+    )
+
+    favorite_color = forms.CharField(
+        label="What is your favorite color?",
+        max_length=80,
+        required=True,
+    )
+
+    favorite_number = forms.IntegerField(
+        label="Favorite number",
+        required=False,
+    )
+
+    notes = forms.CharField(
+        label="Additional notes or feedback",
+        required=False,
+    )
+
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-ContactForm'
+        self.helper.form_class = 'blueForms'
+        self.helper.form_method = 'post'
+        self.helper.form_action = reverse('polls:contact')
+        self.helper.add_input(Submit('submit', 'Submit'))
+        super(ContactForm, self).__init__(*args, **kwargs)
--- src/webui/settings.py
+++ src/webui/settings.py
@@ -86,4 +86,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap'
+CRISPY_FAIL_SILENTLY = not DEBUG
