3e9aa5afc09c4855695bb0e6f1c3befb5d04df3e
Pierre-Yves Strub <pierre-yves@strub.nu>
2014-09-01 15:37:59 +0200

    Fix compilation issues

--- src/ecProvers.ml
+++ src/ecProvers.ml
@@ -98,14 +98,14 @@ let test_if_evict_prover tests prover =
 
 (* -------------------------------------------------------------------- *)
 module Evictions = struct
-  let alt_ergo_lw_0_95_2 = {
+  let alt_ergo_le_0_95_2 = {
     pe_cause = `Inconsistent;
     pe_test  = `ByVersion ("Alt-Ergo", (`Le, VP.of_tuple (0, 95, 2)));
   }
 end
 
 let evictions : prover_eviction_test list = [
-  Evictions.alt_ergo_lt_0_95_2;
+  Evictions.alt_ergo_le_0_95_2;
 ]
 
 (* -------------------------------------------------------------------- *)
