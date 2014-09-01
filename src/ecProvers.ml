dc863d322c7e63a7e186241b9d3e658c170ec17e
Pierre-Yves Strub <pierre-yves@strub.nu>
2014-09-01 15:36:53 +0200

    Alt-Ergo eviction (<= 0.95.2 instead of < 0.96)

--- src/ecProvers.ml
+++ src/ecProvers.ml
@@ -98,14 +98,14 @@ let test_if_evict_prover tests prover =
 
 (* -------------------------------------------------------------------- *)
 module Evictions = struct
-  let alt_ergo_lt_0_96 = {
+  let alt_ergo_lw_0_95_2 = {
     pe_cause = `Inconsistent;
-    pe_test  = `ByVersion ("Alt-Ergo", (`Lt, VP.of_tuple (0, 96, 0)));
+    pe_test  = `ByVersion ("Alt-Ergo", (`Le, VP.of_tuple (0, 95, 2)));
   }
 end
 
 let evictions : prover_eviction_test list = [
-  Evictions.alt_ergo_lt_0_96;
+  Evictions.alt_ergo_lt_0_95_2;
 ]
 
 (* -------------------------------------------------------------------- *)
