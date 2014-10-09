c968b578a4bdfb811301005cb2a3831f983f7d49
Pierre-Yves Strub <pierre-yves@strub.nu>
2014-10-09 18:01:54 +0200

    Uniformize module restriction errors [ref #17037]

--- src/ecTyping.ml
+++ src/ecTyping.ml
@@ -781,7 +781,6 @@ let check_sig_mt_cnv env sin tyout =
   check_sig_cnv `Sub env sin sout
 
 (* -------------------------------------------------------------------- *)
-
 type restriction_who =
 | RW_mod of EcPath.mpath
 | RW_fun of EcPath.xpath 
@@ -794,35 +793,52 @@ type restriction_err =
 | RE_MMissingRestriction  of EcPath.mpath * EcPath.mpath pair
 
 type restriction_error = restriction_who * restriction_err
-  
+
 exception RestrictionError of restriction_error
 
-let pp_restriction_error env fmt (w,e) = 
+let pp_restriction_error env fmt (w, e) = 
   let ppe = EcPrinting.PPEnv.ofenv env in
   let pp_v fmt xp = EcPrinting.pp_pv ppe fmt (pv_glob xp) in
   let pp_m fmt m  = EcPrinting.pp_topmod ppe fmt m in
+
   let pp_restriction_who fmt = function
-    | RW_mod mp -> Format.fprintf fmt "The module %a" pp_m mp
-    | RW_fun xp -> Format.fprintf fmt "The procedure %a" (EcPrinting.pp_funname ppe) xp in
+    | RW_mod mp ->
+        Format.fprintf fmt "the module %a" pp_m mp
+
+    | RW_fun xp ->
+        Format.fprintf fmt "the procedure %a" (EcPrinting.pp_funname ppe) xp in
+
   let pp_restriction_err fmt = function
     | RE_UseVariable x -> 
-      Format.fprintf fmt "cannot uses the variable %a" pp_v x
-    | RE_UseVariableViaModule (x,m) -> 
-      Format.fprintf fmt "uses module %a which uses %a" pp_m m pp_v x
+        Format.fprintf fmt
+          "should not be able to use the variable %a"
+          pp_v x
+
+    | RE_UseVariableViaModule (x, m) -> 
+        Format.fprintf fmt
+          "should not be able to use %a (via %a)"
+          pp_v x pp_m m
+
     | RE_UseModule m -> 
-      Format.fprintf fmt "cannot uses the module %a" pp_m m
-    | RE_VMissingRestriction (x,(m1,m2))->
-      Format.fprintf fmt "can uses %a, add restriction %a to %a"
-        pp_v x pp_m m1 pp_m m2
-    | RE_MMissingRestriction (m,(m1,m2))->
-      Format.fprintf fmt "can uses %a, add restriction %a to %a or %a to %a"
-        pp_m m pp_m m1 pp_m m2 pp_m m2 pp_m m1
-  in
-  Format.fprintf fmt "%a %a" pp_restriction_who w pp_restriction_err e
+        Format.fprintf fmt
+          "should not be able to use the module %a"
+          pp_m m
+
+    | RE_VMissingRestriction (x, (m1, m2))->
+        Format.fprintf fmt
+          "should not be able to use %a, add restriction %a to %a"
+          pp_v x pp_m m1 pp_m m2
+
+    | RE_MMissingRestriction (m, (m1, m2))->
+        Format.fprintf fmt
+          "should not be able to use %a, add restriction %a to %a or %a to %a"
+          pp_m m pp_m m1 pp_m m2 pp_m m2 pp_m m1
+
+  in Format.fprintf fmt "%a %a" pp_restriction_who w pp_restriction_err e
 
 (* -------------------------------------------------------------------- *)
 let check_restrictions env who use restr =
-  let re_error = fun x -> raise (RestrictionError(who, x)) in
+  let re_error = fun x -> raise (RestrictionError (who, x)) in
 
   let restr = NormMp.norm_restr env restr in
 
