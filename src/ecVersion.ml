cd3ed7217c74cb03a9a4b4b8100ad7ead052c2e4
Pierre-Yves Strub <pierre-yves@strub.nu>
2014-10-09 11:31:26 +0200

    Fix `EcVersion module out-of-sync with current licence`

--- src/ecVersion.ml
+++ src/ecVersion.ml
@@ -5,4 +5,4 @@
 
 (* -------------------------------------------------------------------- *)
 let copyright = "Copyright (c) - 2012-2014 - IMDEA Software Institute and INRIA"
-let license   = "Distributed under the terms of the CeCILL-B license"
+let license   = "Distributed under the terms of the CeCILL-C license"
