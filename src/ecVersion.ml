a0a31929c9e153aab46c85295478b3a905867f92
Pierre-Yves Strub <pierre-yves@strub.nu>
2015-02-20 15:30:23 +0100

    Update internal copyright.

--- src/ecVersion.ml
+++ src/ecVersion.ml
@@ -4,5 +4,5 @@
  * -------------------------------------------------------------------- *)
 
 (* -------------------------------------------------------------------- *)
-let copyright = "Copyright (c) - 2012-2014 - IMDEA Software Institute and INRIA"
+let copyright = "Copyright (c) - 2012-2015 - IMDEA Software Institute and INRIA"
 let license   = "Distributed under the terms of the CeCILL-C license"
