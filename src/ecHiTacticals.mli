17cc307b322386e36419ad5732dad017582c6748
Pierre-Yves Strub <pierre-yves@strub.nu>
2016-01-03 11:14:14 +0100

    COPYRIGHT (2016 / headers)

--- src/ecHiTacticals.mli
+++ src/ecHiTacticals.mli
@@ -1,6 +1,6 @@
 (* --------------------------------------------------------------------
- * Copyright (c) - 2012--2015 - IMDEA Software Institute
- * Copyright (c) - 2012--2015 - Inria
+ * Copyright (c) - 2012--2016 - IMDEA Software Institute
+ * Copyright (c) - 2012--2016 - Inria
  * 
  * Distributed under the terms of the CeCILL-C-V1 license
  * -------------------------------------------------------------------- *)
