f8394a1d9acbbc302f9fc836f9c06158431562ed
Pierre-Yves Strub <pierre-yves@strub.nu>
2016-01-31 20:47:20 +0100

    Vagrant: define 2 machines
     - easycrypt: shared folder, not X11, public base box
     - easycrypt-gui: cloned easycrypt, X11 (unity), home-made base box

--- Vagrantfile
+++ Vagrantfile
@@ -6,14 +6,24 @@ Vagrant.configure(2) do |config|
     vb.memory = 6144 # set VM memory to 6GB
   end
 
-  config.vm.synced_folder ".", "/home/vagrant/#{project_name}"
+  config.vm.define "easycrypt", primary: true do |config|
+    config.vm.box = "ubuntu/wily64"
+    config.vm.synced_folder ".", "/home/vagrant/#{project_name}"
 
-  config.vm.define :ubuntu do |ubuntu_config|
-    ubuntu_config.vm.box = "ubuntu/wily64"
+    config.vm.provision "shell", binary: true, privileged: false do |s|
+      s.path = "scripts/vagrant/post-installation.sh"
+      s.args = ["#{project_name}", "shared"]
+    end
   end
 
-  config.vm.provision "shell", binary: true, privileged: false do |s|
-    s.path = "scripts/vagrant/post-installation.sh"
-    s.args = ["#{project_name}"]
+  config.vm.define "easycrypt-gui", autostart: false do |config|
+    config.vm.box       = "easycrypt"
+    config.ssh.username = "easycrypt"
+    config.ssh.password = "easycrypt"
+
+    config.vm.provision "shell", binary: true, privileged: false do |s|
+      s.path = "scripts/vagrant/post-installation.sh"
+      s.args = ["#{project_name}", "cloned"]
+    end
   end
 end
--- scripts/vagrant/post-installation.sh
+++ scripts/vagrant/post-installation.sh
@@ -6,18 +6,20 @@ set -e
 msg () { echo $* >&2; }
 
 # --------------------------------------------------------------------
-pname="$1"
+[ $# -eq 2 ] || exit 1
+
+pname="$1"; mode="$2"
 
 # --------------------------------------------------------------------
 msg "Installing dependencies from Ubuntu packages."
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install \
-  -qq -yy m4 opam libgmp-dev libpcre3-dev pkg-config emacs24
+  -yy git m4 opam libgmp-dev libpcre3-dev pkg-config emacs24
 
 # --------------------------------------------------------------------
 msg "Initializing opam."
 
-opam init -q -a --compiler=4.02.1
+opam init -a --compiler=4.02.1
 eval `opam config env`
 
 opam repository add easycrypt git://github.com/EasyCrypt/opam.git
@@ -26,25 +28,30 @@ opam update
 # --------------------------------------------------------------------
 msg "Installing EasyCrypt and dependencies from OPAM packages."
 
-opam install easycrypt ec-provers ec-proofgeneral
+opam install ec-toolchain ec-provers
 why3 config --detect
-cat >/home/vagrant/.emacs <<EOF
+
+# --------------------------------------------------------------------
+if [ "$mode" = "cloned" ]; then
+  msg "Installing EasyCrypt from opam."
+
+  opam install -q -y easycrypt ec-proofgeneral
+
+  cat >/home/easycrypt/.emacs <<EOF
 (when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
   (setq proof-output-tooltips nil)
 (load-file "~/.opam/4.02.1/share/ec-proofgeneral/generic/proof-site.el")
 EOF
 
-# # --------------------------------------------------------------------
-# msg "Installing EasyCrypt dependencies from OPAM packages."
-# 
-# opam install -q -y ec-toolchain ec-provers
-# why3 config --detect
-# 
-# # --------------------------------------------------------------------
-# msg "Building EasyCrypt."
-# 
-# make -C "${pname}"
-# make -C "${pname}/proofgeneral" local
+  msg "Your vagrant box is ready to be exported."
+fi
 
 # --------------------------------------------------------------------
-msg "Now use vagrant ssh -- -X and make pg"
+if [ "$mode" = "shared" ]; then
+  msg "Building EasyCrypt from shared directory."
+
+  make -C "${pname}"
+  make -C "${pname}/proofgeneral" local
+
+  msg "Now use vagrant ssh -- -X and make pg"
+fi
