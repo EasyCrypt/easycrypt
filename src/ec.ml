bd4c7066f9126df927439eceb3ed9eab191c1d4a
Pierre-Yves Strub <pierre-yves@strub.nu>
2018-03-21 10:06:16 +0100

    New command: why3config

--- src/ec.ml
+++ src/ec.ml
@@ -33,7 +33,8 @@ let copyright =
 let psep = match Sys.os_type with "Win32" -> ";" | _ -> ":"
 
 (* -------------------------------------------------------------------- *)
-let confname = "easycypt.conf"
+let confname    = "easycypt.conf"
+let why3dflconf = Filename.concat XDG.home "why3.conf"
 
 (* -------------------------------------------------------------------- *)
 type pconfig = {
@@ -171,20 +172,22 @@ let main () =
   in
 
   (* Initialize why3 engine *)
-  let why3conf =
+  let cp_why3conf ~exists : string option =
     match options.o_options.o_why3 with
     | None ->
         let confs =
           XDG.Config.file
-            ~exists:true ~mode:`All ~appname:EcVersion.app "why3.conf"
+            ~exists ~mode:`All ~appname:EcVersion.app "why3.conf"
         in List.nth_opt confs 0
 
-    | Some _ as aout -> aout
+    | Some _ as aout -> aout in
 
+  let why3conf = cp_why3conf ~exists:true
   and ovrevict = options.o_options.o_ovrevict in
 
   begin
-    try  EcProvers.initialize ~ovrevict ?why3conf ()
+    try
+      EcProvers.initialize ~ovrevict ?why3conf ()
     with e ->
       Format.eprintf
         "cannot initialize Why3 engine: %a@."
@@ -218,6 +221,26 @@ let main () =
         } in
 
         print_config config; exit 0
+
+    | `Why3Config -> begin
+        let conf = cp_why3conf ~exists:false in
+
+        let () =
+          let ulnk = conf |> odfl why3dflconf in
+          try Unix.unlink ulnk with Unix.Unix_error _ -> ()
+        in
+
+        let pid =
+          let args = ["why3"; "config"; "--detect"] in
+          let args = args @ (conf |> omap (fun x -> ["-C"; x])|> odfl []) in
+
+          Printf.eprintf "Executing: %s\n%!" (String.concat " " args);
+          Unix.create_process "why3" (Array.of_list args)
+            Unix.stdin Unix.stdout Unix.stderr
+        in
+
+        exit (fst (Unix.waitpid [] pid))
+      end
 
     | `Cli cliopts -> begin
         let terminal =
