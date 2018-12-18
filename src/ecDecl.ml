0b59cac4113d6f6d16acdb679d82d5647a9e1bc2
Pierre-Yves Strub <pierre-yves@strub.nu>
2018-12-18 15:34:23 +0100

    fix bug in LDecl where the type of the operator was not correct.

--- src/ecDecl.ml
+++ src/ecDecl.ml
@@ -257,7 +257,7 @@ let axiomatized_op ?(nargs = 0) ?(nosmt = false) path (tparams, bd) =
 
   let opargs = List.map (fun (x, ty) -> f_local x (gty_as_ty ty)) args in
   let tyargs = List.map (EcTypes.tvar |- fst) axpm in
-  let op     = f_op path tyargs axbd.EcCoreFol.f_ty in
+  let op     = f_op path tyargs (toarrow (List.map f_ty opargs) axbd.EcCoreFol.f_ty) in
   let op     = f_app op opargs axbd.f_ty in
   let axspec = f_forall args (f_eq op axbd) in
 
