8d34a21c96736323346a29cf6a374531ab9543e2
Benjamin Lipp (blipp) <blipp@mailbox.org>
2019-03-04 14:39:31 +0100

    WIP: Make Nix derivation work on NixOS 18.09

--- default.nix
+++ default.nix
@@ -6,13 +6,26 @@ let why3_for_ec = stdenv.lib.overrideDerivation why3 (o: {
     url = "https://gforge.inria.fr/frs/download.php/file/36398/why3-0.87.3.tar.gz";
     sha256 = "1fn9v6w1ilkrm2n4rz31w8qvjnchyvwxiqs67z3f59b5k99wb2ka";
   };
+  buildInputs = (with ocaml-ng.ocamlPackages_4_04; [
+    ocaml findlib lablgtk ocamlgraph zarith menhir ]) ++
+    stdenv.lib.optionals (ocamlPackages.ocaml == coq.ocaml ) [
+      coq coq.camlp5
+    ];
 });
 in
 
-stdenv.mkDerivation {
-  name = "easycrypt-1.0";
+stdenv.mkDerivation rec {
+  name = "easycrypt-${version}";
+  version = "1.0";
   src = ./.;
-  buildInputs = [ ]
-    ++ (with ocamlPackages; [ ocaml findlib ocamlbuild batteries menhir merlin zarith inifiles why3_for_ec ])
-    ;
+  buildInputs = [ z3 alt-ergo ]
+  ++ (with ocaml-ng.ocamlPackages_4_04; [ ocaml findlib ocamlbuild batteries menhir merlin zarith inifiles why3_for_ec ])
+  ++ (with python27Packages; [ python pyyaml ]);
+  makeFlags = [ "PREFIX=$(out)" ];
+  preBuild = ''
+    patchShebangs scripts/install
+    patchShebangs scripts/testing
+    patchShebangs scripts/packaging/emacs-based/scripts
+    patchShebangs scripts/docker/test-box/hooks/build
+  '';
 }
