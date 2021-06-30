489ff1728c70bb1ca06f3bba914ec7d3b1a9b3d5
Benjamin Gregoire <Benjamin.Gregoire@inria.fr>
2021-06-30 11:54:24 +0200

    default.nix

--- default.nix
+++ default.nix
@@ -1,19 +1,13 @@
 with import <nixpkgs> {};
 
-let why314 = why3.overrideAttrs (x:
-   { name = "why3.14";
-     src = fetchurl {
-       url = "https://gforge.inria.fr/frs/download.php/file/38425/why3-1.4.0.tar.gz";
-       sha256 = "0lw0cpx347zz9vvwqibmbxgs80fsd16scgk3isscvwxnajpc3rv8";
-      };
-     postPatch = "";
-   }); in
-
+if !lib.versionAtLeast why3.version "1.4" then
+  throw "please update your nixpkgs channel: nix-channel --update"
+else
 stdenv.mkDerivation {
   name = "easycrypt-1.0";
   src = ./.;
-  buildInputs = [ ]
-    ++ (with ocamlPackages; [ ocaml findlib ocamlbuild batteries menhir menhirLib merlin zarith inifiles why314 yojson])
+  buildInputs = [ why3 ]
+    ++ (with ocamlPackages; [ ocaml findlib ocamlbuild batteries menhir menhirLib merlin zarith inifiles yojson])
     ;
   installFlags = [ "PREFIX=$(out)" ];
 }
