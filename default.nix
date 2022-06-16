b7653953f88064c19e36437c980819d8a439a9ce
Pierre-Yves Strub <pierre-yves@strub.nu>
2022-06-16 09:41:17 +0200

    [nix] force Why3 1.4.1

--- default.nix
+++ default.nix
@@ -3,6 +3,18 @@ with import <nixpkgs> {};
 if !lib.versionAtLeast why3.version "1.4" then
   throw "please update your nixpkgs channel: nix-channel --update"
 else
+let why3_local =
+  if !lib.versionOlder why3.version "1.5" then
+  why3.overrideAttrs (o: rec {
+    version = "1.4.1";
+    src = fetchurl {
+      url = "https://why3.gitlabpages.inria.fr/releases/${o.pname}-${version}.tar.gz";
+      sha256 = "sha256:1rqyypzlvagrn43ykl0c5wxyvnry5fl1ykn3xcvlzgghk96yq3jq";
+    };
+  })
+  else why3
+; in
+let why3 = why3_local; in
   stdenv.mkDerivation {
     name = "easycrypt-1.0";
     src = ./.;
