0e3584f08fba75e351e7b13798573bfaad120015
Benjamin Gregoire <Benjamin.Gregoire@inria.fr>
2022-10-07 12:06:23 +0200

    doCheck = false, for merlin (solve a pb with ld warning on mac)

--- default.nix
+++ default.nix
@@ -15,10 +15,16 @@ let why3_local =
   else why3
 ; in
 let why3 = why3_local; in
+
+let merlin = 
+    ocamlPackages.merlin.overrideAttrs (o: {
+    doCheck = false;
+    }); in
+
   stdenv.mkDerivation {
     name = "easycrypt-1.0";
     src = ./.;
-    buildInputs = [ why3 ] ++ (with ocamlPackages; [
+    buildInputs = [ why3 merlin] ++ (with ocamlPackages; [
       ocaml
       findlib
       batteries
@@ -29,7 +35,6 @@ let why3 = why3_local; in
       inifiles
       menhir
       menhirLib
-      merlin
       yojson
       zarith
     ]);
