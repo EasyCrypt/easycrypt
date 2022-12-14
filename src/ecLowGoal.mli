796cc2d1127fac39bebf3e0ed243b47eb761896d
Benjamin Gregoire <Benjamin.Gregoire@inria.fr>
2022-12-14 10:37:13 +0100

    [tactic]: fix t_solve performance issues

--- src/ecLowGoal.mli
+++ src/ecLowGoal.mli
@@ -318,4 +318,9 @@ type smtmode = [`Standard | `Strict | `Report of EcLocation.t option]
 val t_smt: mode:smtmode -> prover_infos -> FApi.backward
 
 (* -------------------------------------------------------------------- *)
-val t_solve : ?canfail:bool -> ?bases:symbol list -> ?depth:int -> FApi.backward
+val t_solve :
+     ?canfail:bool
+  -> ?bases:symbol list
+  -> ?mode:EcMatching.fmoptions
+  -> ?depth:int
+  -> FApi.backward
