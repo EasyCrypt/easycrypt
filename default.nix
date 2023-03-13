0f16a48c5cc5134bd72b0a6f92f5787bc27f60b1
François Dupressoir <fdupress@gmail.com>
2023-03-13 15:32:35 +0000

    [nix] pin the Why3 version
    This makes it easier to bump versions and rely on CI.

--- default.nix
+++ default.nix
@@ -2,6 +2,17 @@
 
 with import <nixpkgs> {};
 
+let why3_local =
+  why3.overrideAttrs (o : rec {
+    version = "1.5.1";
+    src = fetchurl {
+      url = "https://why3.gitlabpages.inria.fr/releases/${o.pname}-${version}.tar.gz";
+      sha256 = "sha256-vNR7WeiSvg+763GcovoZBFDfncekJMeqNegP4fVw06I=";
+    };
+  });
+in
+let why3 = why3_local; in
+
 let provers =
   if withProvers then [
     alt-ergo
