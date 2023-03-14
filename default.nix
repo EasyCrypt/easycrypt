472a2d4f25a98707fe2b2435b1b1e5a4206f5521
François Dupressoir <fdupress@gmail.com>
2023-03-14 14:34:41 +0000

    bump Why3 version in .nix files

--- default.nix
+++ default.nix
@@ -4,10 +4,10 @@ with import <nixpkgs> {};
 
 let why3_local =
   why3.overrideAttrs (o : rec {
-    version = "1.5.1";
+    version = "1.6.0";
     src = fetchurl {
       url = "https://why3.gitlabpages.inria.fr/releases/${o.pname}-${version}.tar.gz";
-      sha256 = "sha256-vNR7WeiSvg+763GcovoZBFDfncekJMeqNegP4fVw06I=";
+      sha256 = "sha256-hFvM6kHScaCtcHCc6Vezl9CR7BFbiKPoTEh7kj0ZJxw=";
     };
   });
 in
--- shell.nix
+++ shell.nix
@@ -1,17 +1,17 @@
 { withProvers ? true, devDeps ? [] }:
 
+with import <nixpkgs> {};
+
 let why3_local =
   why3.overrideAttrs (o : rec {
-    version = "1.5.1";
+    version = "1.6.0";
     src = fetchurl {
       url = "https://why3.gitlabpages.inria.fr/releases/${o.pname}-${version}.tar.gz";
-      sha256 = "sha256-vNR7WeiSvg+763GcovoZBFDfncekJMeqNegP4fVw06I=";
+      sha256 = "sha256-hFvM6kHScaCtcHCc6Vezl9CR7BFbiKPoTEh7kj0ZJxw=";
     };
   });
 in
 let why3 = why3_local; in
-
-with import <nixpkgs> {};
 
 let provers =
   if withProvers then [
