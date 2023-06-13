e38fac31c9e63882810688b55e7a62e0ddf8cd41
Cameron Low <cameronlow@mail.com>
2023-06-13 21:55:00 +0100

    Keep f_let_simp

--- src/ecFol.ml
+++ src/ecFol.ml
@@ -502,6 +502,55 @@ and f_real_inv_simpl f =
        | _ -> destr_error "destr_rint/inv"
      with DestrError _ -> f_app fop_real_inv [f] treal
 
+(* -------------------------------------------------------------------- *)
+let rec f_let_simpl lp f1 f2 =
+  match lp with
+  | LSymbol (id, _) -> begin
+      match Mid.find_opt id (f_fv f2) with
+      | None   -> f2
+      | Some i ->
+          if   i = 1 || can_subst f1
+          then
+            let s = Fsubst.f_bind_local Fsubst.f_subst_id id f1 in
+            Fsubst.f_subst s f2
+          else
+            f_let lp f1 f2
+    end
+
+  | LTuple ids -> begin
+      match f1.f_node with
+      | Ftuple fs ->
+          let (d, s) =
+            List.fold_left2 (fun (d, s) (id, ty) f1 ->
+              match Mid.find_opt id (f_fv f2) with
+              | None   -> (d, s)
+              | Some i ->
+                  if   i = 1 || can_subst f1
+                  then (d, Fsubst.f_bind_local s id f1)
+                  else (((id, ty), f1) :: d, s))
+              ([], Fsubst.f_subst_id) ids fs
+          in
+            List.fold_left
+              (fun f2 (id, f1) -> f_let (LSymbol id) f1 f2)
+              (Fsubst.f_subst s f2) d
+      | _ ->
+        let x = EcIdent.create "tpl" in
+        let ty = ttuple (List.map snd ids) in
+        let lpx = LSymbol(x,ty) in
+        let fx = f_local x ty in
+        let tu = f_tuple (List.mapi (fun i (_,ty') -> f_proj fx i ty') ids) in
+        f_let_simpl lpx f1 (f_let_simpl lp tu f2)
+    end
+
+  | LRecord (_, ids) ->
+      let check (id, _) =
+        id |> omap (fun id -> not (Mid.mem id (f_fv f2))) |> odfl true
+      in if List.for_all check ids then f2 else f_let lp f1 f2
+
+let f_lets_simpl =
+  (* FIXME : optimize this *)
+  List.fold_right (fun (lp,f1) f2 -> f_let_simpl lp f1 f2)
+
 let rec f_app_simpl f args ty =
   f_betared (f_app f args ty)
 
