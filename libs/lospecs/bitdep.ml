2da55f4b194cbe18bb28797a252f71cf9a7b5571
Gustavo Delerue <up201804883@fc.up.pt>
2023-12-20 00:01:06 +0000

    Added bitdep temp var propagation and map dependency generation

--- libs/lospecs/bitdep.ml
+++ libs/lospecs/bitdep.ml
@@ -10,13 +10,31 @@ let rec bd_aexpr (e: aexpr) : deps =
   | ECast (e_, t_) -> bd_aexpr e_ (* fix semantic for non identity casts *)
   | EVar v -> (match t_ with | `W n -> (copy ~offset:(0) ~size:(n) (Ident.name v)) | _ -> failwith "Vars should be words") 
   | EInt i -> empty ~size:(256) (* Need to know how to handle this case, probably good enough for now *)
-  | ESlide (eb, (eo, cnt, sz)) -> 
+  | ESlide (eb, (eo, cnt, sz)) -> (* verify indianess on this *)
       (match eo.node with
-      | EInt i -> failwith "Need to implement fixed base slices"
-      | _ -> bd_aexpr eb) (* Need to check how to handle variable offsets *)
+      | EInt i -> eb |> bd_aexpr |> offset ~offset:(-i) |> restrict ~min:(0) ~max:(cnt*sz)
+      | _ -> 
+        let n = (match eb.type_ with | `W n -> n | _ -> failwith "cant var slice int") 
+        in let bdb = eb 
+          |> bd_aexpr 
+          |> chunk ~csize:(n) ~count:(1) 
+        in let bdo = eo
+          |> bd_aexpr 
+          |> (let rec bitlen (m:int) = (match m with | 1 -> 1 | m -> 1 + bitlen (m/2)) in
+          collapse ~csize:(bitlen n) ~count:(1)) |> Map.Int.find 0 |> constant ~size:(n) 
+          (* Best guess without specific knowledge, result depends on ceil(log2(base_size)) bits of index *)
+        in merge bdb bdo) (* Need to check how to handle variable offsets *)
   (* Map should gen deps for params in terms of fb and then chunk those and sub params for args *)
   (* temp sol is to just chunk full dep *)
-  | EMap ((`W n, `W m), (params, fb), args) -> chunk ~csize:(n) ~count:(m) (bd_aexpr fb)
+  | EMap ((`W n, `W m), (params, fb), args) -> 
+      let bdfb = bd_aexpr fb in
+      let bdargs = List.map bd_aexpr args in
+      let subs = List.combine params bdargs in
+      0 --^ m 
+        |> Enum.map (fun i ->
+          List.fold_left (fun d (v, t) -> propagate ~offset:(i*n) (v |> fst |> Ident.name) t d) bdfb subs 
+          |> offset ~offset:(i*n))
+        |> Enum.fold merge (empty ~size:0)
   | EConcat (`W n, args) -> 
       (match (List.hd args).type_ with
       | `W m -> aggregate ~csize:(m) (Enum.map bd_aexpr (List.enum (List.rev args)))
@@ -57,6 +75,14 @@ let rec bd_aexpr (e: aexpr) : deps =
       | `H -> (fun d -> d |> restrict ~min:(n) ~max:(2*n) |> offset ~offset:(-n))
       | `L -> restrict ~min:(0) ~max:(n))
   | _ -> failwith "Not implemented yet"
+
+  (* propagate v deps to t deps in d *)
+and propagate ~(offset:int) (v: symbol) (t: deps) (d: deps) : deps =
+  Map.Int.map (fun d1 -> 
+    match (Map.String.find_opt v d1) with
+    | None -> d1
+    | Some si -> si |> Set.Int.enum 
+      |> Enum.fold (fun acc i -> merge1 acc (Option.default (Map.String.empty) (Map.Int.find_opt (i + offset) t))) (Map.String.empty)) d
 
 let bd_adef (df: adef) =
   bd_aexpr df.body
