ba9f44fd2a0c539ad9c3dcc6c720ec5fcc43bafa
Pierre-Yves Strub <pierre-yves@strub.nu>
2024-01-08 17:06:56 +0100

    fix

--- libs/lospecs/lospecs_test.ml
+++ libs/lospecs/lospecs_test.ml
@@ -5,7 +5,7 @@ open Lospecs
 let _ =
   let prog = Io.parse IO.stdin in
   let ast = Typing.tt_program Typing.Env.empty prog in
-  let dep = List.map Bitdep.bd_adef ast in
+  let _dep = List.map Bitdep.bd_adef (List.map snd ast) in
   Format.eprintf "%a@."
     (Yojson.Safe.pretty_print ~std:true)
     (Ptree.pprogram_to_yojson prog)
