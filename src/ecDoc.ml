4f472761b498543f9345b11a850659a7b99e3e79
MM45 <m.c.f.h.p.meijers@tue.nl>
2024-07-01 12:33:11 +0200

    Adjustments for styling.

--- src/ecDoc.ml
+++ src/ecDoc.ml
@@ -23,8 +23,33 @@ let itemkind_str_pl (ik : itemkind) : string =
   | `Theory -> "Theories"
 
 let c_head (tstr : string) : [> Html_types.head] elt =
-  head (title (txt tstr)) []
+  head (title (txt tstr)) [link ~rel:[`Stylesheet] ~href:"styles.css" ()]
 
+let c_sidebar (th : string) (lents : EcScope.docentity list) =
+  let iks = [`Type; `Operator; `Axiom; `Lemma; `ModuleType; `Module; `Theory] in
+  let iksin = 
+    List.filter (fun ik -> 
+      List.exists (fun ldoc ->
+        match ldoc with
+        | ItemDoc (_, (_, ikp, _, _)) -> ikp = ik
+        | SubDoc ((_, (_, ikp, _, _)), _) -> ikp = ik) lents) iks
+  in
+    nav ~a:[a_class ["sidebar"]] 
+        [
+          div ~a:[a_class["sidebar-title"]] 
+          [
+            h2 [txt "EasyCrypt Documentation"]; 
+            span ~a:[a_class ["sidebar-title-theory"]] [txt th]
+          ];
+          div ~a:[a_class ["sidebar-elems"]] 
+          [
+            ul ~a:[a_class ["sidebar-section-list"]]
+                (List.map (fun ik -> 
+                  let ikstr = itemkind_str_pl ik in
+                  li [a ~a:[a_href (Xml.uri_of_string ("#" ^ ikstr))] [txt ikstr]]) iksin)
+          ]
+        ]
+  
 let c_section_intro (gdoc : string list) =
   match gdoc with
   | [] -> []
@@ -53,8 +78,10 @@ let c_section_main_itemkind_li ?(supthf : string option) (th : string) (lent_ik 
            | Some supf -> c_filename ?ext:(Some ".html") [supf; th; subth]
           in
           li ~a:[a_id (itemkind_str_pl ik ^ subth); a_class ["item-entry"]] ([
-            div ~a:[a_class ["item-name"]] [a ~a:[a_href (Xml.uri_of_string hn)] [txt subth]]; 
-            div ~a:[a_class ["item-basic-desc"]] [txt hdoc]
+            div ~a:[a_class ["item-name-desc-container"]] [  
+              div ~a:[a_class ["item-name"]] [a ~a:[a_href (Xml.uri_of_string hn)] [txt subth]]; 
+              div ~a:[a_class ["item-basic-desc"]] [txt hdoc]
+            ]
           ] @ (if tdoc <> []
                then [details ~a:[a_class ["item-details"]] (summary [])
                              (List.map (fun d -> p ~a:[a_class ["item-details-par"]] [txt d]) tdoc)]
@@ -72,8 +99,10 @@ let c_section_main_itemkind_li ?(supthf : string option) (th : string) (lent_ik 
               else List.hd doc, List.tl doc
             in
             li ~a:[a_class ["item-entry"]] [
-              div ~a:[a_class ["item-name"]] [txt nm]; 
-              div ~a:[a_class ["item-basic-desc"]] [txt hdoc]; 
+              div ~a:[a_class ["item-name-desc-container"]] [
+                div ~a:[a_class ["item-name"]] [txt nm]; 
+                div ~a:[a_class ["item-basic-desc"]] [txt hdoc]
+              ]; 
               details ~a:[a_class ["item-details"]] (summary [])
                       (List.map (fun d -> p ~a:[a_class ["item-details-par"]] [txt d]) tdoc 
                        @ [div ~a:[a_class ["source-container"]] 
@@ -108,23 +137,27 @@ let c_section_main ?(supthf : string option) (th : string) (lents : EcScope.doce
     iks)
 
 let c_body ?(supths : string option) ?(supthf : string option) (th : string) (tstr : string) (gdoc : string list) (ldocents : EcScope.docentity list) : [> Html_types.body] elt =
-  let page_heading = h1 ~a:[a_class ["page-heading"]] [txt tstr] ::  
-    match supths with
-    | None -> []
-    | Some sup ->
-          match supthf with
-          | None -> assert false
-          | Some supf ->  
-              [
-                h2 ~a:[a_class ["page-subheading"]] [
-                  txt ("Subtheory of ");
-                  a ~a:[a_href (Xml.uri_of_string (supf ^ ".html" ^ "#" ^ itemkind_str_pl `Theory ^ th))] [txt sup]
-                ]
-              ]
+  let sidebar = c_sidebar th ldocents in
+  let page_heading = 
+    div ~a:[a_class ["page-heading-container"]]
+      (h1 ~a:[a_class ["page-heading"]] [txt tstr] 
+        ::  
+        match supths with
+        | None -> []
+        | Some sup ->
+              match supthf with
+              | None -> assert false
+              | Some supf ->  
+                  [
+                    h2 ~a:[a_class ["page-subheading"]] [
+                      txt ("Subtheory of ");
+                      a ~a:[a_href (Xml.uri_of_string (supf ^ ".html" ^ "#" ^ itemkind_str_pl `Theory ^ th))] [txt sup]
+                    ]
+                  ])
   in
-  let intro = c_section_intro gdoc in
-  let main = c_section_main ?supthf th ldocents in
-  body (page_heading @ intro @ main)
+  let sec_intro = c_section_intro gdoc in
+  let sec_main = c_section_main ?supthf th ldocents in
+  body (sidebar :: [main (page_heading :: sec_intro @ sec_main)])
 
 let c_page ?(supths : string option) ?(supthf : string option) (th : string) (tstr : string) (gdoc : string list) (ldocents : EcScope.docentity list) : [> Html_types.html] elt =
     html (c_head tstr) (c_body ?supths ?supthf th tstr gdoc ldocents)
