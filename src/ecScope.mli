fa186703edd54815b930dd865e8ef5f5f0a3d6bc
Pierre-Yves Strub <pierre-yves.strub@pqshield.com>
2025-01-20 11:02:05 +0100

    Rework import mechanism.
    The import mechanism is currently brittle, buggy, interact wrongly
    with the cloning mechanism. Moreover, some declarations duplicate it
    by reimplementing an ad-hoc, "non-import" mechanism. This commit fixes
    this issues.

--- src/ecScope.mli
+++ src/ecScope.mli
@@ -50,8 +50,8 @@ and proof_state =
   PSNoCheck | PSCheck of EcCoreGoal.proof
 
 and pucflags = {
-  puc_visibility : EcDecl.ax_visibility;
-  puc_local      : bool;
+  puc_smt   : bool;
+  puc_local : bool;
 }
 
 (* -------------------------------------------------------------------- *)
@@ -119,7 +119,7 @@ module Ty : sig
   val add : scope -> ptydecl located -> scope
 
   val add_class    : scope -> ptypeclass located -> scope
-  val add_instance : ?import:EcTheory.import -> scope -> Ax.proofmode -> ptycinstance located -> scope
+  val add_instance : scope -> Ax.proofmode -> ptycinstance located -> scope
 end
 
 (* -------------------------------------------------------------------- *)
@@ -151,7 +151,8 @@ module Theory : sig
   (* [exit scope] close and finalize the top-most theory and returns
    * its name. Raises [TopScope] if [scope] has not super scope. *)
   val exit :
-       ?pempty:[`ClearOnly | `Full | `No]
+       ?import:bool
+    -> ?pempty:[`ClearOnly | `Full | `No]
     -> ?clears:(pqsymbol option) list
     -> scope -> symbol * scope
 
