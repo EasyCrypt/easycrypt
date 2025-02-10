ecb33e4f155b2e3565f48415c573d2eec2a5f0a0
Cameron Low <cameronlow121@gmail.com>
2025-02-10 14:34:54 +0000

    Reworked outline and function unification. No longer need to provide left-values, instead use the alias mode via '~'.

--- src/ecUnifyProc.mli
+++ src/ecUnifyProc.mli
@@ -3,24 +3,15 @@ open EcModules
 open EcSymbols
 
 (*---------------------------------------------------------------------------------------*)
-(* `Unification` of procedures *)
-(*
-  Given: r <@ foo(a1: t1, a2: t2, ...); and s1; s2; ...; sr.
-  Attempt to find values for a1, a2, ... such that, the body of `foo` with a1, a2, ...
-  replaced will exactly match s1; s2; ..., and that `r <- res` match sr.
-  Where `res` is the return expression of `foo`.
-
-  Currently, this is done by traversing the respective ASTs and when a relevant
-  program variable is encountered on the lhs, use the rhs expression.
-
-  FIXME: This is incredibly basic and should be iterated on with a more advanced
-  procedure unification algorithm.
- *)
-
-(*---------------------------------------------------------------------------------------*)
 type u_error =
+  | UE_AliasNoRet
+  | UE_AliasNotPv
   | UE_InvalidRetInstr
-  | UE_UnexpectedReturn
+  | UE_AbstractFun
+  | UE_TypeMismatch of expr * expr
+  | UE_UnificationFailArg of symbol
+  | UE_UnificationFailPv of symbol
+  | UE_LvNotInLockstep of lvalue * lvalue
   | UE_ExprNotInLockstep of expr * expr
   | UE_InstrNotInLockstep of instr * instr
   | UE_DifferentProgramLengths of stmt * stmt
@@ -28,16 +19,18 @@ type u_error =
 exception UnificationError of u_error
 
 (*---------------------------------------------------------------------------------------*)
-type u_value =
-  | Empty
-  | Fixed of expr
+(* `Unification` of procedures *)
+(*
+  The basic problem is:
+    Given `r <@ f(a1, ..., an);` and a statement `s`, attempt to find values for
+    `r`, `a1`,..., `an` such that inlining `f` will exactly match s.
 
-type umap = u_value Msym.t
+  There are two modes of operation:
+  - Exact: assume `s` is of the form `b`; `r <- e;` and perform unification of
+    `b` with `f.body` and `e` with `f.res`.
+  - Alias: assume `f.ret` is a program variable or tuple of program variables, only
+    perform unification with `s` and `f.body`.
 
-(*---------------------------------------------------------------------------------------*)
-(* The following functions attempt to unify unknown program variables
-   in the lhs with expressions from the rhs *)
-val unify_exprs : umap -> expr -> expr -> umap
-val unify_instrs : umap -> instr -> instr -> umap
-val unify_stmts : umap -> stmt -> stmt -> umap
-val unify_func : umap -> function_def -> stmt -> instr option -> lvalue option * umap
+  Both modes result in a closed function call instruction `r' <@ f(a'1, ..., a'n);`
+ *)
+val unify_func : EcEnv.env -> [`Exact | `Alias] -> EcPath.xpath -> stmt -> stmt
