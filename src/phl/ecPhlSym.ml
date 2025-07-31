1b06f4ff5101ce5780924d104e811f228bfb060e
Oskar Goldhahn <oskar.goldhahn@ntnu.no>
2025-07-31 16:01:19 +0200

    fix symmetry

--- src/phl/ecPhlSym.ml
+++ src/phl/ecPhlSym.ml
@@ -6,29 +6,21 @@ open EcSubst
 open EcAst
 
 (*-------------------------------------------------------------------- *)
-let build_sym ml mr pr po =
-  let s = Fsubst.f_subst_id in
-  let s = Fsubst.f_bind_mem s ml mr in
-  let s = Fsubst.f_bind_mem s mr ml in
-  let s = Fsubst.f_subst s in
-  (s pr, s po)
-
-(*-------------------------------------------------------------------- *)
 let t_equivF_sym tc =
   let ef    = tc1_as_equivF tc in
-  let ml, mr = ef.ef_ml, ef.ef_mr in
-  let pr    = ts_inv_rebind (ef_pr ef) mr ml in
-  let po    = ts_inv_rebind (ef_po ef) mr ml in
+  let mr, ml = ef.ef_ml, ef.ef_mr in
+  let pr    = {ml;mr;inv=(ef_pr ef).inv} in
+  let po    = {ml;mr;inv=(ef_po ef).inv} in
   let cond  = f_equivF pr ef.ef_fr ef.ef_fl po in
   FApi.xmutate1 tc `EquivSym [cond]
 
 (*-------------------------------------------------------------------- *)
 let t_equivS_sym tc =
   let es    = tc1_as_equivS tc in
-  let (ml, mtl), (mr, mtr) = es.es_ml, es.es_mr in
-  let pr    = ts_inv_rebind (es_pr es) mr ml in
-  let po    = ts_inv_rebind (es_po es) mr ml in
-  let cond  = f_equivS mtr mtl pr es.es_sr es.es_sl po in
+  let (mr, mtr), (ml, mtl) = es.es_ml, es.es_mr in
+  let pr    = {ml;mr;inv=(es_pr es).inv} in
+  let po    = {ml;mr;inv=(es_po es).inv} in
+  let cond  = f_equivS mtl mtr pr es.es_sr es.es_sl po in
 
   FApi.xmutate1 tc `EquivSym [cond]
 
