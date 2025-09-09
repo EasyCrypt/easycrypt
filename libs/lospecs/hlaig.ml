7a9c4d06dd29bacd1721c845295362e96c27e5a3
Gustavo Delerue <gxdelerue@proton.me>
2025-09-09 11:00:09 +0100

    Lospecs cleanup

--- libs/lospecs/hlaig.ml
+++ libs/lospecs/hlaig.ml
@@ -1,7 +1,6 @@
 type node = Aig.node
 type reg = Aig.reg
 
-type input = string * int
 (* tdeps : int -> int set ; dependency for a single output bit 
            i |->  {j | output depends on bit j of var i }*)
 type tdeps = (int, int Set.t) Map.t
@@ -272,68 +271,6 @@ let makeBWZinterface () : (module SMTInterface) =
   (module MakeSMTInterface ((val makeBWZinstance () : SMTInstance)))
 
 
-module Env : sig
-  type env
-
-  val id : env -> string -> env * int
-  val get : env -> string -> int
-  val get_opt : env -> string -> int option
-  val get_reverse : env -> int -> string 
-  val get_reverse_opt : env -> int -> string option
-  val input_of_var : env -> Aig.var -> input
-  val var_of_input : env -> input -> Aig.var
-  val empty: env
-  val namer_of_env : env -> (int -> string)
-
-end = struct
-  type env = {
-    forward : (string, int) Map.t;
-    reverse : (int, string) Map.t;
-  }
-
-  let fresh = 
-    let counter = ref 0 in
-    fun () -> incr counter; !counter 
-
-  let empty = {
-      forward = Map.empty;
-      reverse = Map.empty;
-    }
-
-  let id (env: env) (s: string) =
-    match Map.find_opt s env.forward with
-    | None ->
-      let id = fresh () in
-      ({ forward = Map.add s id env.forward;
-         reverse = Map.add id s env.reverse; }, id)
-    | Some i -> (env, i)
-
-  let get (env: env) (s: string) = 
-    match Map.find_opt s env.forward with
-    | Some i -> i
-    | None -> Format.eprintf "Variable %s not in map@." s; assert false
-
-  let get_opt (env: env) (s: string) = 
-    Map.find_opt s env.forward
-
-  let get_reverse (env: env) (i: int) = 
-    Map.find i env.reverse
-
-  let get_reverse_opt (env: env) (i: int) = 
-    Map.find_opt i env.reverse
-
-  let input_of_var (env: env) ((v, i): Aig.var) =
-    (get_reverse env v,i)
-
-  let var_of_input (env: env) ((s, i): input) =
-    (get env s, i)
-
-  let namer_of_env (env: env) : int -> string =
-    fun i -> get_reverse env i
-end
-
-type env = Env.env
-
 let of_int (i:int) : reg = 
   (* Number of bits the integer occupies *)
   let rec log2up (i: int) : int = 
@@ -342,29 +279,6 @@ let of_int (i:int) : reg =
   | _ -> 1 + log2up (i/2) 
   in
   Circuit.of_int ~size:(log2up i) i
-
-
-(* -------------------------------------------------------------------- *)
-let input (env: env) ((s,i) : input) =
-  let (env, v) = 
-    match Env.get_opt env s with
-    | Some v -> (env, v)
-    | None -> Env.id env s
-  in (env, Aig.input (v, i))
-
-let reg (env: env) ~(size : int) ~(name : string) : env * reg =
-  let (env, id) = Env.id env name in
-  (env, Circuit.reg ~size:size ~name:id)
-
-(* Checks that variable v does not appear in the circuit  *)
-let check_sub (env: env) (v: string) (circ: reg) : bool =
-  let rec doit (c: node) : bool = 
-    match c.gate with
-    | False -> true
-    | And (n1, n2) -> (doit n1) && (doit n2)
-    | Input (c, i) -> (Env.get_reverse env c) <> v
-  in
-  List.for_all doit circ
 
 (* ------------------------------------------------------------------------------- *)
 (* FIXME: CHECK THIS *)
@@ -392,75 +306,7 @@ let rec inputs_of_node : _ -> Aig.var Set.t =
 let inputs_of_reg (r : Aig.reg) : Aig.var Set.t =
   List.fold_left (fun acc x -> Set.union acc (inputs_of_node x)) Set.empty r
 
-(* -------------------------------------------------------------------- *)
-(* ?? *)
-let aenv_of_regs (env: env) (rs : bytes list) = 
-  fun inp -> 
-  Aig.env_of_regs rs @@ Env.var_of_input env inp
-
 (* ==================================================================== *)
-(* check this one *)
-let map (env: env) (aenv : input -> node option) : node -> node =
-  let _env = fun var ->
-    aenv @@ Env.input_of_var env var
-  in Aig.map _env 
-
-let rename_input (env: env) (source: string) (target: string) (offset: int) : node -> env * node =
-  let cache : (int, node) Hashtbl.t = Hashtbl.create 0 in
-
-  let rec doit (env: env) (n : node) : env * node =
-    let env, mn =
-      match Hashtbl.find_option cache (abs n.id) with
-      | None ->
-        let env, mn = doit_r env n.gate in
-        Hashtbl.add cache (abs n.id) mn;
-        env, mn
-      | Some mn ->
-        env, mn
-    in
-      env, if 0 < n.id then mn else Aig.neg mn
-
-  and doit_r (env: env) (n : Aig.node_r) =
-    match n with
-    | False ->
-      (env, Aig.false_)
-    | Input ((v, i) as orig) -> begin
-      match Env.get_reverse_opt env v with
-      | Some n when n = source -> 
-        input env (target, i+offset)
-      | _ -> (env, Aig.input orig)
-      end
-    | And (n1, n2) -> 
-      let env, n1 = doit env n1 in
-      let env, n2 = doit env n2 in
-      env, Aig.and_ n1 n2
-
-  in fun (n : node) -> doit env n
-
-let rename_input_s (env: env) (source: string) (target: string) (offset: int): reg -> env * reg =
-  fun r -> List.fold_left_map (fun env node -> rename_input env source target offset node) env r
-
-(* -------------------------------------------------------------------- *)
-let maps (env:env) (aenv : input -> node option) : reg -> reg =
-  fun r -> List.map (map env aenv) r  
-
-(* ==================================================================== *)
-let equivs (env: env) (inputs : (input * input) list) (c1 : reg) (c2 : reg) : bool =
-  let inputs = List.map (fun (a,b) -> (Env.var_of_input env a, Env.var_of_input env b)) inputs in
-  Aig.equivs inputs c1 c2
-
-(* ==================================================================== *)
-let eval (env: env) (aenv : input -> bool) =
-  let aenv = fun var ->
-  aenv @@ Env.input_of_var env var
-  in Aig.eval aenv 
-   
-(* -------------------------------------------------------------------- *)
-let evals (env: env) (aenv : input -> bool) = 
-  let aenv = fun var ->
-  aenv @@ Env.input_of_var env var
-  in Aig.evals aenv 
-
 let rec dep : _ -> tdeps = 
   let cache : (int, tdeps) Hashtbl.t = Hashtbl.create 0 in
 
@@ -570,7 +416,8 @@ let collapse_blocks (d: tdblock list) : tdblock option =
     (Some h) t
 
 (* -------------------------------------------------------------------- *)
-let rec pp_node ?(namer=string_of_int) (env: env) (fmt : Format.formatter) (n : node) =
+let rec pp_node ?(namer=string_of_int) (fmt : Format.formatter) (n : node) =
+  let pp_node = pp_node ~namer in
   match n with
   | { gate = False; id } when 0 < id ->
     Format.fprintf fmt "⊥"
@@ -584,10 +431,10 @@ let rec pp_node ?(namer=string_of_int) (env: env) (fmt : Format.formatter) (n : 
       (if 0 < id then "" else "¬") s i
 
   | { gate = And (n1, n2); id; } when 0 < id ->
-    Format.fprintf fmt "(%a) ∧ (%a)" (pp_node env) n1 (pp_node env) n2
+    Format.fprintf fmt "(%a) ∧ (%a)" pp_node n1 pp_node n2
 
   | { gate = And (n1, n2); } ->
-    Format.fprintf fmt "¬((%a) ∧ (%a))" (pp_node env) n1 (pp_node env) n2
+    Format.fprintf fmt "¬((%a) ∧ (%a))" pp_node n1 pp_node n2
 
 let pp_dep ?(namer = string_of_int) (fmt : Format.formatter) (d: tdeps) : unit =
   let print_set fmt s = Set.iter (Format.fprintf fmt "%d ") s in
@@ -608,67 +455,3 @@ let zpad (n: int) (r: Aig.reg)  =
   if List.length r < n then
     List.append r (List.init (n - (List.length r)) (fun _ -> Aig.false_))
   else r
-
-let single_var_equiv_precheck (env: env) (r1: reg) (r2: reg) : bool =
-  let (r1, r2) = if List.compare_lengths r1 r2 < 0 then
-    (zpad (List.length r2) r1, r2) else
-    (r1, zpad (List.length r1) r2)
-  in
-
-  let d1 = deps r1 |> Array.to_list |> tdblock_of_tdeps in 
-  let d2 = deps r2 |> Array.to_list |> tdblock_of_tdeps in
-  if not (compare_tdblocks d1 d2) 
-  then false
-  else true 
-  (* FIXME: find some better way of doing this *)
-
-
-
-(* ------------------------------------------------------------------------------- *)
-(* Assuming dependence on only one variable for now *)
-let circ_equiv (env: env) (r1 : Aig.reg) (r2 : Aig.reg) : bool = 
-  (* Format.eprintf "CIRC_EQUIV: @."; *)
-  assert (single_var_equiv_precheck env r1 r2);
-  (* Format.eprintf "%a@." (pp_deps) (deps env r1 |> Array.to_list); *)
-  (* Format.eprintf "%a@." (pp_deps) (deps env r2 |> Array.to_list); *)
-  let inp1 = inputs_of_reg r1 |> Set.to_list in
-  let inp2 = inputs_of_reg r2 |> Set.to_list in
-  let inps = List.combine (List.take (List.length inp2) inp1) (List.take (List.length inp1) inp2) in
-  Aig.equivs inps r1 r2
-
-(* let bruteforce (r : Aig.reg) (vars : Aig.var list) : unit = *) 
-  (* let rec doit (acc : bool list) (n : int) : unit = *)
-    (* match n with *)
-    (* | 0 -> let eval = ((List.combine vars acc) |> List.to_seq |> Map.of_seq) in *)
-           (* let eval = C.eval (fun x -> Map.find x eval) in *) 
-           (* List.map eval r |> C.uint_of_bools |> Format.eprintf "@.@.ERROR: -> %d: %d@." (C.uint_of_bools acc) *) 
-    (* | n -> doit (false::acc) (n-1); doit (true::acc) (n-1) *)
-
-  (* in doit [] (List.length vars) *)
-
-(* let bools_of_int (n : int) ~(size: int) : bool list = *)
-  (* List.init size (fun i -> ((n lsr i) land 1) <> 0) *) 
-
-(* let bruteforce_equiv (r1 : Aig..reg) (r2 : Aig.reg) (range: int) : bool = *) 
-  
-  (* let (r1, r2) = if List.compare_lengths r1 r2 < 0 then *)
-    (* (zpad (List.length r2) r1, r2) else *)
-    (* (r1, zpad (List.length r1) r2) *)
-  (* in *)
-
-  (* let. eval (r : Aig.reg) (n: int) : int = *)
-    (* let inp = inputs_of_reg r |> Set.to_list in *)
-    (* let vals = bools_of_int n ~size:(List.length inp) in *)
-    (* let env = List.combine inp vals |> List.to_seq |> Map.of_seq in *)
-    (* (*let eval = C.eval (fun x -> Map.find x env) in *)
-    (* List.map eval r *) *)
-    (* let res = C.evals (fun x -> Map.find x env) r |> C.uint_of_bools in *)
-    (* res *)
-  (* in *)
-  (* Enum.(--^) 0 range *) 
-    (* |> Enum.map (fun i -> *) 
-        (* let res1 = (eval r1 i) in *)
-        (* let res2 = (eval r2 i) in *)
-        (* if res1 = res2 then true *) 
-        (* else (Format.eprintf "i: %d | r1: %d | r2: %d@." i res1 res2; false)) |> Enum.fold (&&) true *)
-
