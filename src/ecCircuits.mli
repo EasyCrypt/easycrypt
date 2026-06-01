3779cdd0748959ad2b6ba754b9fe6e6ef33072da
Pierre-Yves Strub <pierre-yves.strub@pqshield.com>
2026-06-01 11:13:33 +0200

    ecCircuits: trim .mli to the externally-used interface
    Of the 33 values exported by ecCircuits.mli, only 14 are used outside the
    module (by ecPhlBDep / ecPhlRwPrgm). The other 19 were either pass-through
    re-exports of EcLowCircuits names or internal helpers needlessly exposed.
    Drop them from the interface (they remain available internally); also
    remove the now-unused 'open EcIdent'/'open EcSymbols'. No behaviour change
    (release + ci builds clean, circuit tests pass).

--- src/ecCircuits.mli
+++ src/ecCircuits.mli
@@ -1,6 +1,4 @@
 (* -------------------------------------------------------------------- *)
-open EcIdent
-open EcSymbols
 open EcAst
 open EcEnv
 open LDecl
@@ -51,59 +49,31 @@ type circuit_error =
 
 exception CircError of circuit_error
 
-val circ_error : circuit_error -> 'a
 val pp_circ_error : EcPrinting.PPEnv.t -> Format.formatter -> circuit_error -> unit
 
 (* -------------------------------------------------------------------- *)
 (* Utilities (figure out better name) *)
 val circ_red : hyps -> EcReduction.reduction_info
-val width_of_type : env -> ty -> int 
-val circuit_to_string : circuit -> string
-val ctype_of_ty : env -> ty -> ctype
 val int_of_form : ?redmode:EcReduction.reduction_info -> hyps -> form -> BI.zint
 
-(* State utilities *)
-val state_get : state -> memory -> symbol -> circuit
-val state_get_opt : state -> memory -> symbol -> circuit option
-val state_get_all : state -> circuit list 
-
-(* Create circuits *)
-val input_of_type : name:[`Str of string | `Idn of ident | `Bad] -> env -> ty -> circuit
-
-(* Transform circuits *)
-val circuit_ueq : circuit -> circuit -> circuit
-val circuit_flatten : circuit -> circuit
-
 (* Use circuits *)
-val compute    : sign:bool -> circuit -> BI.zint list -> BI.zint
-val circ_equiv : ?pcond:circuit -> circuit -> circuit -> bool
-val circ_sat   : circuit -> bool 
-val circ_taut  : circuit -> bool 
+val circ_taut  : circuit -> bool
 
 (* Generate circuits *)
 (* Form processors *)
 val circuit_of_form : state -> hyps -> form -> circuit
 val circuit_check_posts : env:env -> pres:circuit list -> circuit list -> bool
 val circuits_of_equality : st:state -> hyps:hyps -> form -> form -> circuit list
-val circuit_simplify_equality : st:state -> hyps:hyps -> pres:circuit list -> form -> form -> bool
 val circ_simplify_form_bitstring_equality :
   ?st:state ->
   ?pres:circuit list -> hyps -> form -> form
- 
+
 (* Proc processors *)
-val state_of_prog : ?close:bool -> hyps -> memory -> st:state -> instr list -> state 
+val state_of_prog : ?close:bool -> hyps -> memory -> st:state -> instr list -> state
 val instrs_equiv : hyps -> memenv -> ?keep:EcPV.PV.t -> state -> instr list -> instr list -> bool
-val process_instr : hyps -> memory -> st:state -> instr -> state
 
 val circuit_state_of_memenv : ?st:state -> env -> memenv -> state
-val circuit_state_of_hyps : ?st:state -> ?strict:bool -> hyps -> state 
-
-(* Check for uninitialized inputs *)
-val circuit_has_uninitialized : circuit -> int option
-
-val circuit_slice : circuit -> int -> int -> circuit
-
-val circuit_to_file : name:string -> circuit -> symbol
+val circuit_state_of_hyps : ?st:state -> ?strict:bool -> hyps -> state
 
 (* Imperative state clearing *)
 val clear_translation_caches : unit -> unit
